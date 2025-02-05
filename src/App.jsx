ef1521572c2c90f3a43d74ed0a69a34f5e05f696
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-02-05 19:05:34 +0500

    Remove Loader component and simplify App to directly render Background

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
 import Background from "./Components/Background";
-import Loader from "./Components/Loader";
 
 const App = () => {
-      const [isLoading, setIsLoading] = useState(true);
-      window.addEventListener("load", () => {
-            setTimeout(() => {
-                  setIsLoading(false);
-            }, 1000);
-      });
-      return <>{isLoading ? <Loader /> : <Background />}</>;
+      return <Background />;
 };
 
 export default App;
