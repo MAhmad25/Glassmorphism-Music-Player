f619dc38ccc9d3c8a44f1d68d148006a8234c460
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-02-23 00:22:30 +0500

    Refactor MusicList component to adjust position dynamically and clean up unused code

--- src/Components/MusicList.jsx
+++ src/Components/MusicList.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/prop-types */
 import MusicItems from "./MusicItems";
 import { motion } from "motion/react";
+import { useState } from "react";
 import { MdOutlineClose } from "react-icons/md";
 
 const MusicList = ({ nav, songs, playingMusicInfo, runMusic }) => {
+      // const [position, setPosition] = useState(0);
+      // window.addEventListener("resize", (e) => {
+      //       console.log(position);
+      //       if (e.target.screen.width - 6 >= 640) {
+      //             if (position < 0) {
+      //                   setPosition(100);
+      //             }
+      //       }
+      // });
       return (
-            <motion.div className={`w-full nav right-[-100%] sm:right-[0%] relative h-full cursor-pointer overflow-x-hidden overflow-y-scroll  p-5 sm:bg-transparent ${playingMusicInfo.bgColor}`}>
+            <motion.div className={`w-full nav sm:right-[${position}%] relative h-full cursor-pointer overflow-x-hidden overflow-y-scroll  p-5 sm:bg-transparent ${playingMusicInfo.bgColor}`}>
                   <h1 className={`text-4xl mb-2 font-semibold tracking-tight ${playingMusicInfo.textColor} rounded-3xl sm:hidden  py-2 leading-none uppercase`}>PlayList</h1>
                   <motion.span
                         whileTap={{ scale: 0.7 }}
