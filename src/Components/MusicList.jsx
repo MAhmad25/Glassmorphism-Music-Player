41e7b89fffecb1d59f50920aceeb7f2eb6e0842c
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-02-23 19:49:59 +0500

    Implement window resize handling in MusicList component for responsive navigation

--- src/Components/MusicList.jsx
+++ src/Components/MusicList.jsx
@@ -1,19 +1,17 @@
 /* eslint-disable react/prop-types */
 import MusicItems from "./MusicItems";
 import { motion } from "motion/react";
-// import { useState } from "react";
+import { useEffect } from "react";
 import { MdOutlineClose } from "react-icons/md";
 
 const MusicList = ({ nav, songs, playingMusicInfo, runMusic }) => {
-      // const [position, setPosition] = useState(0);
-      // window.addEventListener("resize", (e) => {
-      //       console.log(position);
-      //       if (e.target.screen.width - 6 >= 640) {
-      //             if (position < 0) {
-      //                   setPosition(100);
-      //             }
-      //       }
-      // });
+      useEffect(() => {
+            const handleResize = () => {
+                  if (window.innerWidth >= 640) nav.current.style.right = "0%";
+            };
+            window.addEventListener("resize", handleResize);
+            return () => removeEventListener("resize", handleResize);
+      }, [nav]);
       return (
             <motion.div className={`w-full nav sm:right-[0%] relative h-full cursor-pointer overflow-x-hidden overflow-y-scroll  p-5 sm:bg-transparent ${playingMusicInfo.bgColor}`}>
                   <h1 className={`text-4xl mb-2 font-semibold tracking-tight ${playingMusicInfo.textColor} rounded-3xl sm:hidden  py-2 leading-none uppercase`}>PlayList</h1>
