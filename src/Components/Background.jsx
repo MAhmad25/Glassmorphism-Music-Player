05436a9ca63b5c7a22e4f799602a14b7f8b40ae4
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-03-12 20:16:08 +0500

    Refactor Background and MusicItems components for improved structure and animation effects

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -5,7 +5,7 @@ import { PlayingMusic } from "../Contexts/PlayingMusic";
 const Background = () => {
       const { playingMusicInfo } = useContext(PlayingMusic);
       return (
-            <section className={`w-full bg-center overflow-hidden relative bg-cover h-svh`}>
+            <section className={`w-full  bg-center overflow-hidden relative bg-cover h-svh`}>
                   {playingMusicInfo ? <img className="w-full h-full object-cover absolute top-0 left-0" src={playingMusicInfo.image} alt="" /> : "Loading"}
                   <Foreground />
             </section>
--- src/Components/MusicItems.jsx
+++ src/Components/MusicItems.jsx
@@ -3,11 +3,13 @@ import { motion } from "motion/react";
 /* eslint-disable react/prop-types */
 const MusicItems = ({ musicDetail }) => {
       return (
-            <motion.div initial={{ x: "-110%" }} animate={{ x: 0, transition: { duration: 0.7, delay: 0.2, ease: "backInOut" } }} whileTap={{ paddingLeft: "60px" }} whileHover={{ paddingLeft: "20px" }} className="w-full py-5 px-2 gap-5 flex items-center border-b-[.5px]">
-                  <div className="w-10 h-10 overflow-hidden rounded-md">
+            <motion.div initial={{ x: "-110%" }} animate={{ x: 0, transition: { duration: 0.7, delay: 0.2, ease: "backInOut" } }} whileHover={{ paddingLeft: "20px" }} className="w-full py-5 px-2 gap-5 flex items-center border-b-[.5px]">
+                  <motion.div whileTap={{ opacity: 0, scale: 2 }} className="w-10 h-10 overflow-hidden rounded-md">
                         <img className="w-full h-full object-cover" src={musicDetail.image} alt="" />
-                  </div>
-                  <h1 className="text-xl leading-none">{musicDetail.title}</h1>
+                  </motion.div>
+                  <motion.h1 whileTap={{ opacity: 0, scale: 1 }} className="text-xl leading-none">
+                        {musicDetail.title}
+                  </motion.h1>
             </motion.div>
       );
 };
