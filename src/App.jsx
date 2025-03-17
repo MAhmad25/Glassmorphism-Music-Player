57fffcc937b91401f0189d52de34133bf6d09100
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-03-17 21:39:50 +0500

    Add @vercel/speed-insights dependency and integrate into App component for performance tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Background from "./Components/Background";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 const App = () => {
       return (
             <>
                   <Analytics />
+                  <SpeedInsights />
                   <Background />
             </>
       );
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import MusicPlaying from "./Contexts/PlayingMusic.jsx";
-
 createRoot(document.getElementById("root")).render(
       <MusicPlaying>
             <App />
