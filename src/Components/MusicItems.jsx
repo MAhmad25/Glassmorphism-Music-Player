3755459caa336cf8ac5c254376ead4e924b06693
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-06-02 22:39:38 +0500

    Refactor MusicItems component to simplify animations and improve hover effects

--- src/Components/MusicItems.jsx
+++ src/Components/MusicItems.jsx
@@ -3,7 +3,7 @@ import { motion } from "motion/react";
 /* eslint-disable react/prop-types */
 const MusicItems = ({ musicDetail }) => {
       return (
-            <motion.div initial={{ x: "-110%" }} animate={{ x: 0, transition: { duration: 0.7, delay: 0.2, ease: "backInOut" } }} whileHover={{ paddingLeft: "20px" }} className="w-full py-5 px-2 gap-5 flex items-center border-b-[.5px]">
+            <motion.div whileHover={{ paddingLeft: "20px" }} className="w-full py-5 px-2 gap-5 flex items-center border-b-[.5px]">
                   <motion.div whileTap={{ opacity: 0, scale: 2 }} className="w-10 h-10 overflow-hidden rounded-md">
                         <img className="w-full h-full object-cover" src={musicDetail.image} alt="" />
                   </motion.div>
