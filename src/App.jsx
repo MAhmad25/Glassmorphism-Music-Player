9581aee6ec30ee4f55108c3d39da89c765e5e52a
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-06-27 18:00:41 +0500

    Remove @vercel/speed-insights dependency and its usage from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Background from "./Components/Background";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 const App = () => {
       return (
             <>
                   <Analytics />
-                  <SpeedInsights />
                   <Background />
             </>
       );
