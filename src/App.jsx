fd0c03b74b66da26c2bd6ac65aca18284bdc79eb
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-07-22 12:45:37 +0500

    Refactor App component by removing loading state management and deleting Mice component, simplifying the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
-import { useState, useEffect } from "react";
 import Background from "./Components/Background";
 import { Analytics } from "@vercel/analytics/react";
-import Mice from "./Components/Mice";
 const App = () => {
-      const [appLoaded, setAppLoaded] = useState(false);
-      useEffect(() => {
-            const onLoad = () => setAppLoaded(true);
-            if (document.readyState === "complete") {
-                  onLoad();
-            } else {
-                  window.addEventListener("load", onLoad);
-                  return () => window.removeEventListener("load", onLoad);
-            }
-      }, []);
-
       return (
             <>
-                  {appLoaded ? (
-                        <>
-                              <Analytics />
-                              <Background />
-                        </>
-                  ) : (
-                        <Mice />
-                  )}
+                  <Analytics />
+                  <Background />
             </>
       );
 };
